425d3d3077048fff89daf8b6130eccb47ab424c2
장혜민 <ham@janghyemin-ui-MacBookPro.local>
2023-02-02 10:28:42 +0900

    Initial commit

---
+++ src/main/java/com/jojoldu/book/treelecspringboot2webservice/TreelecSpringboot2WebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.treelecspringboot2webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreelecSpringboot2WebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreelecSpringboot2WebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jojoldu/book/treelecspringboot2webservice/TreelecSpringboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jojoldu.book.treelecspringboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreelecSpringboot2WebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
